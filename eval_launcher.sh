b4430869fcab63cf85c9da3118b83e97a76cdeb4
Tsach <tsach276@mit.edu>
2023-12-21 10:59:10 -0500

    added a feature to take multiple samples from the xrd diffusion

--- eval_launcher.sh
+++ eval_launcher.sh
@@ -5,4 +5,4 @@ export JOB_NAME="$2"
 
 # Run the main script with sbatch, setting the job name, output, and error files using the exported variable
 #note: the third argument is the number of batches to run
-sbatch --job-name="${JOB_NAME}" --output="output/${JOB_NAME}_%j_output.txt" --error="error/${JOB_NAME}_%j_error.txt" --export=ALL my_python_runner_gpu_eval.sh "$1" "$3" "$4"
+sbatch --job-name="${JOB_NAME}" --output="output/${JOB_NAME}_%j_output.txt" --error="error/${JOB_NAME}_%j_error.txt" --export=ALL my_python_runner_gpu_eval.sh "$1" "$3" "$4" "$5"
